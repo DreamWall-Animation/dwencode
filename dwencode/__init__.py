19e42f6447a663860d5d297b85beb09b04aa50c0
Olivier Evers <evers@dreamwall.be>
2021-07-08 10:08:10 +0200

    absolute imports

--- dwencode/__init__.py
+++ dwencode/__init__.py
@@ -2,5 +2,5 @@ __author__ = 'Olivier Evers'
 __copyright__ = 'DreamWall'
 __license__ = 'MIT'
 
-from encode import encode
-from concatenate import concatenate_videos
+from dwencode.encode import encode
+from dwencode.concatenate import concatenate_videos
