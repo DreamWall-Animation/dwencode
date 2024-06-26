a87bd10fe7c4b794317c1d8edd342f8f28285e85
Olivier E <ndeebook@hotmail.com>
2024-06-26 14:32:20 +0200

    change get_ffmpeg_executable_path

--- dwencode/ffpath.py
+++ dwencode/ffpath.py
@@ -1,8 +1,4 @@
-
 import os
-import logging
-import platform
-import subprocess
 from functools import lru_cache
 
 
@@ -12,21 +8,18 @@ CREATE_NO_WINDOW = 0x08000000
 @lru_cache()
 def get_ffmpeg_executable_path(name='ffmpeg', path=None):
     executable_name = f'{name}.exe' if os.name == 'nt' else name
-    if path and os.path.isdir(path):
-        path = f'{path}/{executable_name}'
-    if path and not os.path.exists(path):
-        logging.warning(f'"{path}" does not exist.')
-    path = path or name
-    try:
-        if platform.system() == 'Windows':
-            subprocess.check_call(
-                f'{path} -version',
-                creationflags=CREATE_NO_WINDOW)
-        else:
-            subprocess.check_call([path, '-version'])
-    except subprocess.CalledProcessError:
+    if path:
+        if os.path.isdir(path):
+            path = f'{path}/{executable_name}'.replace('\\', '/')
+        if not os.path.exists(path):
+            raise Exception(f'"{name}" not found.')
+        return path
+    else:
+        for path in os.environ['PATH'].split(os.pathsep):
+            path = f'{path}/{executable_name}'.replace('\\', '/')
+            if os.path.exists(path):
+                return path
         raise Exception(f'"{name}" not found.')
-    return path
 
 
 def get_ffmpeg_path(path=None):
